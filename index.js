54d8d2b1e5f87a9c1da9420c148b189507ed5d01
Dong Eon Lee <dddlee.dev@gmail.com>
2020-05-15 16:25:56 +0900

    merge 추가

--- index.js
+++ index.js
@@ -8,29 +8,17 @@ var template = require('./lib/template.js');
 
 
 app.use(express.static('img'));
-<<<<<<< HEAD
 app.use(express.static('css'));
 
 var favicon = require('serve-favicon');
 app.use('/img', favicon(path.join(__dirname + '/img','favicon.jpg')));
 
-=======
-
-var favicon = require('serve-favicon');
-app.use(favicon(path.join(__dirname,'/img','favicon.jpg')));
-
-var logo = `<img src="logo.png"></img>`;
->>>>>>> main
 var shopRouter = require('./routes/shop');
 
 app.get('/', function(req, res){
     var title = 'hi';
     var description = "메인화면";
-<<<<<<< HEAD
     var html = template.HTML(title, description)
-=======
-    var html = template.HTML(title, description, logo)
->>>>>>> main
     res.send(html);
 });
 
@@ -44,11 +32,7 @@ app.get('/login/:pageId', function(req, res){
             allowedTags:['h1']
           });
           var html = template.HTML(sanitizedTitle, 
-<<<<<<< HEAD
             `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`
-=======
-            `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`, `<img src="../logo.png"></img>`
->>>>>>> main
           );
         res.send(html);
     });
